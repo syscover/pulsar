95973bcb2ef08ad64218520942cd4521fcb8cbae
Jose Carlos <cpalacin@syscover.com>
2015-02-09 18:52:05 +0100

    update service provider

--- src/Pulsar/Pulsar/PulsarServiceProvider.php
+++ src/Pulsar/Pulsar/PulsarServiceProvider.php
@@ -12,26 +12,26 @@ class PulsarServiceProvider extends ServiceProvider
 	public function boot()
 	{
 		// load autoload from package composer.json
-		require realpath(__DIR__ . '/../vendor/autoload.php');
+		require realpath(__DIR__ . '/../../../vendor/autoload.php');
 
 		// include route.php file
-		include realpath(__DIR__ . '/routes.php');
+		include realpath(__DIR__ . '/../../routes.php');
 
 		// register views
-		$this->loadViewsFrom(realpath(__DIR__ . '/views'), 'pulsar');
+		$this->loadViewsFrom(realpath(__DIR__ . '/../../views'), 'pulsar');
 
 		// register translations
-		$this->loadTranslationsFrom(realpath(__DIR__ . '/lang'), 'pulsar');
+		$this->loadTranslationsFrom(realpath(__DIR__ . '/../../lang'), 'pulsar');
 
 		// register public files
 		$this->publishes([
-			realpath(__DIR__ . '/../public') => public_path('/packages/pulsar/pulsar')
+			realpath(__DIR__ . '/../../../public') => public_path('/packages/pulsar/pulsar')
 		]);
 
 		// register config files
 		$this->publishes([
-			realpath(__DIR__ . '/config/pulsar.php') => config_path('pulsar.php'),
-			realpath(__DIR__ . '/config/auth.php') => config_path('auth.php')
+			realpath(__DIR__ . '/../../config/pulsar.php') => config_path('pulsar.php'),
+			realpath(__DIR__ . '/../../config/auth.php') => config_path('auth.php')
 		]);
 	}
 
