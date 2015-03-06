3226e9ef550c57e2eeaa8f9eda31e1ba77a3c3be
Jose Carlos <cpalacin@syscover.com>
2015-03-06 19:41:55 +0100

    input text template

--- src/views/common/block/block_form_text_group.blade.php
+++ src/views/common/block/block_form_text_group.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group">
     <label class="col-md-{{ $sizeLabel or 2 }} control-label">{{ $label }} @if(isset($required) && $required) @include('pulsar::common.block.block_required') @endif</label>
     <div class="col-md-{{ $sizeField or 10 }}">
-        <input class="form-control{{ isset($required) && $required? ' required' : null }}" type="text" name="{{ $name }}" value="{{ isset($value)? $value : null }}" @if(isset($maxLength)) maxlength="{{ $maxLength }}"@endif @if(isset($rangeLength))rangelength="{{ $rangeLength }}"@endif @if(isset($min)) min="{{ $min }}"@endif{{ isset($readOnly) && $readOnly? ' readonly' : null }}>
+        <input class="form-control{{ isset($required) && $required? ' required' : null }}" type="{{ isset($type)? $type : 'text' }}" name="{{ $name }}" value="{{ isset($value)? $value : null }}" @if(isset($maxLength)) maxlength="{{ $maxLength }}"@endif @if(isset($rangeLength))rangelength="{{ $rangeLength }}"@endif @if(isset($min)) min="{{ $min }}"@endif{{ isset($readOnly) && $readOnly? ' readonly' : null }}>
         {!! $errors->first($name, config('pulsar.errorDelimiters')) !!}
     </div>
 </div>
