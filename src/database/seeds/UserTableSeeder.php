1e503c2c34c6544e48207bbc4995e58f7961e109
Jose Carlos <cpalacin@syscover.com>
2015-03-11 19:01:59 +0100

    lang demo user

--- src/database/seeds/UserTableSeeder.php
+++ src/database/seeds/UserTableSeeder.php
@@ -8,7 +8,7 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         User::insert([
-            ['id_010' => '1','remember_token_010' => NULL,'lang_010' => 'es','profile_010' => '1','access_010' => '1','user_010' => 'admin@pulsar.local','password_010' => '$2y$10$3eFZAd31wPmg2mMZB/CZ4.CkcZKY9xr7A3Z9ou6mp7OkSIc3Qo.yW','email_010' => 'adminn@pulsar.local','name_010' => 'Pulsar','surname_010' => 'Pulsar','created_at' => '0000-00-00 00:00:00','updated_at' => '0000-00-00 00:00:00']
+            ['id_010' => '1','remember_token_010' => NULL,'lang_010' => 'en','profile_010' => '1','access_010' => '1','user_010' => 'admin@pulsar.local','password_010' => '$2y$10$3eFZAd31wPmg2mMZB/CZ4.CkcZKY9xr7A3Z9ou6mp7OkSIc3Qo.yW','email_010' => 'adminn@pulsar.local','name_010' => 'Pulsar','surname_010' => 'Pulsar','created_at' => '0000-00-00 00:00:00','updated_at' => '0000-00-00 00:00:00']
         ]);
     }
 }
