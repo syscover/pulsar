a3e7fb7baaaac95b631f212e16adfeaa25106d76
Jose Carlos <cpalacin@syscover.com>
2015-05-07 14:57:16 +0200

    package change

--- src/database/seeds/PackageTableSeeder.php
+++ src/database/seeds/PackageTableSeeder.php
@@ -11,7 +11,7 @@ class PackageTableSeeder extends Seeder
             ['id_012' => '1','name_012' => 'Pulsar','active_012' => '1'],
             ['id_012' => '2','name_012' => 'Administration Package','active_012' => '1'],
             ['id_012' => '3','name_012' => 'Comunik Package','active_012' => '0'],
-            ['id_012' => '4','name_012' => 'Wordpress Package','active_012' => '0'],
+            ['id_012' => '4','name_012' => 'Forms Package','active_012' => '0'],
             ['id_012' => '5','name_012' => 'Updater Package','active_012' => '0'],
             ['id_012' => '6','name_012' => 'Forms Package','active_012' => '0'],
             ['id_012' => '7','name_012' => 'Hotels Package','active_012' => '0'],
@@ -19,6 +19,8 @@ class PackageTableSeeder extends Seeder
             ['id_012' => '9','name_012' => 'Marketplace Package','active_012' => '0'],
             ['id_012' => '10','name_012' => 'Mi financiacion Package','active_012' => '0'],
             ['id_012' => '11','name_012' => 'Booking Package','active_012' => '0'],
+            ['id_012' => '12','name_012' => 'CRM Package','active_012' => '0'],
+            ['id_012' => '13','name_012' => 'CMS Package','active_012' => '0'],
         ]);
     }
 }
