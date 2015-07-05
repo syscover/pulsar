f867a2dba3398c5be9f734b219c42a1aa2a007b7
Jose Carlos <cpalacin@syscover.com>
2015-07-05 19:37:14 +0200

    idContainer for idContainer

--- src/views/includes/html/form_section_header.blade.php
+++ src/views/includes/html/form_section_header.blade.php
@@ -1,3 +1,3 @@
-<div @if(isset($idContainer)) id="{{ $idContainer }}"@endif class="widget-sub-header">
+<div @if(isset($containerId)) id="{{ $containerId }}"@endif class="widget-sub-header">
     <h4><i class="{{ $icon }}"></i> {{ $label }}</h4>
 </div>
--- src/views/includes/html/form_text_group.blade.php
+++ src/views/includes/html/form_text_group.blade.php
@@ -1,4 +1,4 @@
-<div @if(isset($idContainer)) id="{{ $idContainer }}"@endif class="form-group">
+<div @if(isset($containerId)) id="{{ $containerId }}"@endif class="form-group">
     <label class="col-md-{{ $labelSize or 2 }} control-label">{{ $label }} @if(isset($required) && $required) @include('pulsar::includes.html.required') @endif</label>
     <div class="col-md-{{ $fieldSize or 10 }}">
         <input class="form-control{{ isset($class)? ' ' . $class : null }}" type="{{ isset($type)? $type : 'text' }}" name="{{ $name }}" value="{{ isset($value)? $value : null }}"{!! isset($data)? Miscellaneous::setDataAttributes($data) : null !!} @if(isset($placeholder)) placeholder="{{ $placeholder }}"@endif @if(isset($maxLength)) maxlength="{{ $maxLength }}"@endif @if(isset($rangeLength))rangelength="{{ $rangeLength }}"@endif @if(isset($min)) min="{{ $min }}"@endif{{ isset($readOnly) && $readOnly? ' readonly' : null }}{{ isset($required) && $required? ' required' : null }}>
