f36d4cda087c32d0c20546f6c5c498d0950645f8
Jose Carlos <cpalacin@syscover.com>
2015-07-05 18:42:24 +0200

    wrapperId for containerId

--- src/views/includes/html/form_select_group.blade.php
+++ src/views/includes/html/form_select_group.blade.php
@@ -1,4 +1,4 @@
-<div{!! isset($wrapperId)? ' id="' . $wrapperId . '"' : null !!} class="form-group">
+<div @if(isset($idContainer)) id="{{ $idContainer }}"@endif class="form-group">
     <label{!! isset($labelId)? ' id="' . $labelId . '"' : null !!} class="col-md-{{ $labelSize or 2 }} control-label">{{ isset($label)? $label : null }} @if(isset($required) && $required) @include('pulsar::includes.html.required') @endif</label>
     <div class="col-md-{{ $fieldSize or 10 }}">
         <select{!! isset($id)? ' id="' . $id . '" ' : null !!}{!! isset($class)? ' class="' . $class . '"' : null !!} {!! isset($style)? ' style="' . $style . '" ' : null !!}name="{{ $name }}"{{ isset($multiple)? ' multiple' : null }}{{ isset($required) && $required? ' required' : null }}{!! isset($data)? Miscellaneous::setDataAttributes($data) : null !!}>
