71dee4f5f75913e709f3bafe4b7f00b3761a46c2
Jose Carlos <cpalacin@syscover.com>
2015-09-08 14:32:57 +0200

    common translations

---
+++ src/lang/es/common.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Common Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | Days
+    |--------------------------------------------------------------------------
+    */
+    'monday'                                => 'Lunes',
+    'tuesday'                               => 'Martes',
+    'wednesday'                             => 'Miércoles',
+    'thursday'                              => 'Jueves',
+    'friday'                                => 'Viernes',
+    'saturday'                              => 'Sábado',
+    'sunday'                                => 'Domingo',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Months
+    |--------------------------------------------------------------------------
+    */
+    'january'                               => 'Enero',
+    'february'                              => 'Febrero',
+    'march'                                 => 'Marzo',
+    'april'                                 => 'Abril',
+    'may'                                   => 'Mayo',
+    'june'                                  => 'Junio',
+    'july'                                  => 'Julio',
+    'August '                               => 'Agosto',
+    'September'                             => 'Septiembre',
+    'October'                               => 'Octubre',
+    'November'                              => 'Noviembre',
+    'December'                              => 'Diciembre'
+];
