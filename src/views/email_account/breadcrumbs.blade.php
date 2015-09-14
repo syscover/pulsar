ed67956256f13219c62b9a1419896757bb02aef8
Jose Carlos <cpalacin@syscover.com>
2015-09-14 19:13:41 +0200

    minor fixed

--- src/views/email_account/breadcrumbs.blade.php
+++ src/views/email_account/breadcrumbs.blade.php
@@ -1,6 +1,6 @@
 <!-- pulsar::email_account.breadcrumbs -->
 <li>
-    <a href="javascript:void(0);">{{ trans('pulsar::pulsar.package_name') }}</a>
+    <a href="javascript:void(0);">{{ trans('pulsar::pulsar.administration') }}</a>
 </li>
 <li class="current">
     <a href="{{ route($routeSuffix) }}">{{ trans_choice($objectTrans, 2) }}</a>
