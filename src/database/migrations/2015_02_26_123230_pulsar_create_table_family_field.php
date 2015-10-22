83f63533d6e1a4276921e9a5193dbca0242bc11b
Jose Carlos Rodríguez Palacín <cpalacin@syscover.com>
2015-10-22 18:23:14 +0200

    create table family field

---
+++ src/database/migrations/2015_02_26_123230_pulsar_create_table_family_field.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PulsarCreateTableFamilyField extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('001_025_family_field', function(Blueprint $table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id_025')->unsigned();
+            $table->string('name_025', 100)->nullable();
+            $table->string('resource_025', 30);                             // resource which belong to this family field
+            $table->text('data_025')->nullable();
+
+            $table->foreign('resource_025')->references('id_007')->on('001_007_resource')
+                ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('001_025_family_field');
+    }
+}
