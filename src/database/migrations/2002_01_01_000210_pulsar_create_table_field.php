be4bdd7c7fc448e747dca128ab8dda29a1bb788d
Jose Carlos Rodríguez Palacín <cpalacin@syscover.com>
2015-11-22 21:30:48 +0100

    field create

--- src/database/migrations/2002_01_01_000210_pulsar_create_table_field.php
+++ src/database/migrations/2002_01_01_000210_pulsar_create_table_field.php
@@ -16,12 +16,8 @@ class PulsarCreateTableField extends Migration {
         {
             $table->engine = 'InnoDB';
             $table->increments('id_026')->unsigned();
-            //$table->string('lang_026',2);
             $table->integer('family_026')->unsigned();
             $table->string('name_026', 100)->nullable();
-
-            //$table->string('label_026', 100)->nullable();
-
             $table->tinyInteger('type_026')->unsigned();
             // 1 - text
             // 2 - select
@@ -38,15 +34,13 @@ class PulsarCreateTableField extends Migration {
             $table->smallInteger('sorting_026')->unsigned()->nullable();
             $table->integer('max_length_026')->unsigned()->nullable();
             $table->string('pattern_026', 50)->nullable()->nullable();
-            $table->tinyInteger('label_size_026')->unsigned()->nullable();
-            $table->tinyInteger('col_size_026')->unsigned()->nullable();
+
+            $table->tinyInteger('label_size_026')->unsigned()->nullable();  // column bootstrap size
+            $table->tinyInteger('field_size_026')->unsigned()->nullable();  // column bootstrap size
 
             $table->string('data_lang_026',255)->nullable();
             $table->text('data_026')->nullable();
 
-           // $table->primary(['id_026', 'lang_026']);
-            //$table->foreign('lang_026')->references('id_001')->on('001_001_lang')
-            //    ->onDelete('restrict')->onUpdate('cascade');
             $table->foreign('family_026')->references('id_025')->on('001_025_field_family')
                 ->onDelete('cascade')->onUpdate('cascade');
         });
