31a57c9e175663fc3ef33cfd06646da3aa0b5a25
Jose Carlos Rodríguez Palacín <cpalacin@syscover.com>
2015-11-27 17:32:11 +0100

    tittle library attachments

--- src/views/attachment_library/breadcrumbs.blade.php
+++ src/views/attachment_library/breadcrumbs.blade.php
@@ -1,6 +1,6 @@
 <!-- cms::library.breadcrumbs -->
 <li>
-    <a href="javascript:void(0)">{{ trans('hotels::pulsar.package_name') }}</a>
+    <a href="javascript:void(0)">{{ trans('pulsar::pulsar.package_name') }}</a>
 </li>
 <li class="current">
     <a href="{{ route($routeSuffix) }}">{{ trans_choice($objectTrans, 2) }}</a>
