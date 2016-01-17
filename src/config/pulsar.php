fd98fbef1c6265c9e5b7bf5d59f5c9e819a31b02
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-01-17 18:31:34 +0100

    set new fields

--- src/config/pulsar.php
+++ src/config/pulsar.php
@@ -70,14 +70,16 @@ return [
     //***   Type fields to select on fields section
     //******************************************************************************************************************
     'fieldTypes'                => [
-        (object)['id' => 1,    'key' => 'text',             'name' => 'Text',                'view' => 'pulsar::includes.html.form_text_group'],
-        (object)['id' => 2,    'key' => 'select',           'name' => 'Select',              'view' => 'pulsar::includes.html.form_select_group'],
-        (object)['id' => 3,    'key' => 'selectMultiple',   'name' => 'Select multiple',     'view' => 'pulsar::includes.html.form_select_group'],
-        (object)['id' => 4,    'key' => 'number',           'name' => 'Number',              'view' => 'pulsar::includes.html.form_text_group'],
-        (object)['id' => 5,    'key' => 'email',            'name' => 'Email',               'view' => 'pulsar::includes.html.form_text_group'],
-        (object)['id' => 6,    'key' => 'checkbox',         'name' => 'Checkbox',            'view' => 'pulsar::includes.html.form_checkbox_group'],
-        (object)['id' => 7,    'key' => 'select2',          'name' => 'Select 2',            'view' => 'pulsar::includes.html.form_select_group'],
-        (object)['id' => 8,    'key' => 'selectMultiple2',  'name' => 'Select multiple 2',   'view' => 'pulsar::includes.html.form_select_group'],
+        (object)['id' => 1,    'key' => 'text',             'name' => 'Text',               'view' => 'pulsar::includes.html.form_text_group'],
+        (object)['id' => 2,    'key' => 'select',           'name' => 'Select',             'view' => 'pulsar::includes.html.form_select_group'],
+        (object)['id' => 3,    'key' => 'selectMultiple',   'name' => 'Select multiple',    'view' => 'pulsar::includes.html.form_select_group'],
+        (object)['id' => 4,    'key' => 'number',           'name' => 'Number',             'view' => 'pulsar::includes.html.form_text_group'],
+        (object)['id' => 5,    'key' => 'email',            'name' => 'Email',              'view' => 'pulsar::includes.html.form_text_group'],
+        (object)['id' => 6,    'key' => 'checkbox',         'name' => 'Checkbox',           'view' => 'pulsar::includes.html.form_checkbox_group'],
+        (object)['id' => 7,    'key' => 'select2',          'name' => 'Select 2',           'view' => 'pulsar::includes.html.form_select_group'],
+        (object)['id' => 8,    'key' => 'selectMultiple2',  'name' => 'Select multiple 2',  'view' => 'pulsar::includes.html.form_select_group'],
+        (object)['id' => 9,    'key' => 'textarea',         'name' => 'Text Area',          'view' => 'pulsar::includes.html.form_textarea_group'],
+        (object)['id' => 10,   'key' => 'wysiwyg',          'name' => 'Wysiwyg',            'view' => 'pulsar::includes.html.form_wysiwyg_group'],
     ],
 
     //******************************************************************************************************************
---
+++ src/views/includes/js/custom_fields.blade.php
@@ -0,0 +1,79 @@
+<script type="text/javascript">
+
+    $(document).ready(function() {
+        // hide every elements
+        $('#headerCustomFields').hide()
+        $('#wrapperCustomFields').hide()
+
+        // on change family show fields and custom fields
+        $("[name=customFieldGroup]").on('change', function() {
+            if($("[name=customFieldGroup]").val())
+            {
+                // get html doing a request to controller to render the views
+                @if($action == 'edit' || isset($id))
+                    var request = {
+                            customFieldGroup: $("[name=customFieldGroup]").val(),
+                            lang: '{{ $lang->id_001 }}',
+                            object: '{{ $id }}',
+                            resource: '{{ $resource }}',
+                            action: '{{ $action }}'
+                        }
+                @else
+                    var request = {
+                            customFieldGroup: $("[name=customFieldGroup]").val(),
+                            lang: '{{ $lang->id_001 }}'
+                        }
+                @endif
+
+                $.ajax({
+                    dataType: 'json',
+                    type: 'POST',
+                    headers: {'X-CSRF-TOKEN': '{{ csrf_token() }}'},
+                    url: '{{ route('apiGetCustomFields') }}',
+                    data: request,
+                    success: function (data) {
+                        // set html custom fields section
+                        $('#wrapperCustomFields').html(data.html)
+
+                        if ($.fn.select2)
+                            $('.select2').each(function() {
+                                var self = $(this);
+                                $(self).select2(self.data());
+                            })
+
+                        if($.fn.froalaEditor)
+                            $('.wysiwyg').froalaEditor({
+                                language: '{{ config('app.locale') }}',
+                                toolbarInline: false,
+                                toolbarSticky: true,
+                                tabSpaces: true,
+                                shortcutsEnabled: ['show', 'bold', 'italic', 'underline', 'strikeThrough', 'indent', 'outdent', 'undo', 'redo', 'insertImage', 'createLink'],
+                                toolbarButtons: ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', '|', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'insertHR', 'insertLink', 'undo', 'redo', 'clearFormatting', 'selectAll', 'html'],
+                                heightMin: 130,
+                                enter: $.FroalaEditor.ENTER_BR,
+                                key: '{{ config('pulsar.froalaEditorKey') }}'
+                            })
+
+                        if (data.html != '')
+                        {
+                            $(".uniform").uniform()
+                            $('#headerCustomFields').fadeIn()
+                            $('#wrapperCustomFields').fadeIn()
+                        }
+                    }
+                });
+            }
+            else
+            {
+                $('#headerCustomFields').fadeOut()
+                $('#wrapperCustomFields').fadeOut()
+                $('#wrapperCustomFields').html('')
+            }
+        })
+
+        // if we have customFieldGroup value, throw event to show or hide elements
+        if($("[name=customFieldGroup]").val())
+            $("[name=customFieldGroup]").trigger('change')
+
+    });
+</script>
