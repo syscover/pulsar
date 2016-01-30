731d603097741e7407766678f91bca29faa56eaa
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-01-30 12:19:17 +0100

    set https envairoment

--- src/Syscover/Pulsar/Middleware/HttpsProtocol.php
+++ src/Syscover/Pulsar/Middleware/HttpsProtocol.php
@@ -6,7 +6,7 @@ class HttpsProtocol
 {
     public function handle($request, Closure $next)
     {
-        if (!$request->secure() && env('APP_ENV') === 'prod')
+        if (!$request->secure() && env('APP_ENV') === 'production')
             return redirect()->secure($request->getRequestUri());
 
         return $next($request);
