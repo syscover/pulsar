cd6ad191dedac93063a64c3d15182a5567261705
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-02-25 01:04:46 +0100

    Text table

---
+++ src/Syscover/Pulsar/Models/Text.php
@@ -0,0 +1,39 @@
+<?php namespace Syscover\Pulsar\Models;
+
+use Illuminate\Support\Facades\Validator;
+use Syscover\Pulsar\Traits\TraitModel;
+use Sofa\Eloquence\Eloquence;
+use Sofa\Eloquence\Mappable;
+
+/**
+ * Class Text
+ *
+ * Model with properties
+ * <br><b>[id, name]</b>
+ *
+ * @package     Syscover\Pulsar\Models
+ */
+
+class Text extends Model
+{
+    use TraitModel;
+    use Eloquence, Mappable;
+
+	protected $table        = '001_017_text';
+    protected $primaryKey   = 'id_017';
+    public $incrementing    = false;
+    protected $suffix       = '017';
+    public $timestamps      = false;
+    protected $fillable     = ['id_017', 'lang_017', 'text_017'];
+    protected $maps         = [];
+    private static $rules   = [
+        'lang'   => Lang::class
+    ];
+
+    public static function validate($data, $specialRules = [])
+    {
+        if(isset($specialRules['idRule']) && $specialRules['idRule'])   static::$rules['id'] = 'required|between:2,25';
+
+        return Validator::make($data, static::$rules);
+	}
+}
---
+++ src/database/migrations/2002_01_01_000240_pulsar_create_table_text.php
@@ -0,0 +1,44 @@
+r<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class PulsarCreateTableText extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        if(!Schema::hasTable('001_017_text'))
+        {
+            Schema::create('001_017_text', function (Blueprint $table) {
+                $table->engine = 'InnoDB';
+
+                $table->integer('id_017')->unsigned();
+                $table->string('lang_017', 2);
+                $table->text('text_017')->nullable();
+
+                $table->primary(['id_017', 'lang_017'], 'pk01_001_017_text');
+
+                $table->foreign('lang_017', 'fk01_001_017_text')->references('id_001')->on('001_001_lang')
+                    ->onDelete('restrict')->onUpdate('cascade');
+            });
+        }
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        if (Schema::hasTable('001_017_text'))
+        {
+            Schema::drop('001_017_text');
+        }
+	}
+}
