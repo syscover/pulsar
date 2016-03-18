8e6e49f6a31ffda2bd0bd7d7fe2c8081314397c6
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-03-18 18:11:10 +0100

    add after and $beforeButtonFooter

--- src/views/includes/html/form_record_footer.blade.php
+++ src/views/includes/html/form_record_footer.blade.php
@@ -1,14 +1,20 @@
 <!-- pulsar::includes.html.form_record_footer -->
     <div class="form-actions">
+        @if(isset($buttonFooterBefore))
+            {!! $buttonFooterBefore !!}
+        @endif
         @if($action != 'show')
-        <button type="submit" class="btn marginR10">{{ trans('pulsar::pulsar.save') }}</button>
+            <button type="submit" class="btn marginR10">{{ trans('pulsar::pulsar.save') }}</button>
         @endif
         @if(!isset($cancelButton) || isset($cancelButton) && $cancelButton)
-        <a id="cancel" class="btn btn-inverse" href="{{ route($routeSuffix, $urlParameters) }}">{{ trans('pulsar::pulsar.cancel') }}</a>
+            <a id="cancel" class="btn btn-inverse" href="{{ route($routeSuffix, $urlParameters) }}">{{ trans('pulsar::pulsar.cancel') }}</a>
         @endif
         @if($action != 'store' && isset($lang) && $lang->id_001 != session('baseLang')->id_001)
             <a class="btn btn-danger marginL10 delete-lang-record" data-delete-url="{{ route('deleteTranslation' . ucfirst($routeSuffix), $urlParameters) }}">{{ trans('pulsar::pulsar.delete_translation') }}</a>
         @endif
+        @if(isset($buttonFooterAfter))
+            {!! $buttonFooterAfter !!}
+        @endif
     </div>
 </form>
-<!-- /pulsar::includes.html.form_record_footer -->
+<!-- ./pulsar::includes.html.form_record_footer -->
