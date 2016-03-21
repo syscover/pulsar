c8a9b1e2ba985b2159055fd4505c69d04eb55323
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-03-21 19:48:55 +0100

    minor fix

--- src/views/includes/nav/layout.blade.php
+++ src/views/includes/nav/layout.blade.php
@@ -1,7 +1,13 @@
 <ul id="nav">
     @foreach(session('packages') as $package)
-        @if(session('userAcl')->allows($package->folder_012, 'access') && View::exists($package->folder_012 . '::includes.nav.main'))
-            @include($package->folder_012 . '::includes.nav.main')
+        @if($package->folder_012 == 'pulsar')
+            @if(session('userAcl')->allows('admin', 'access') && View::exists('pulsar::includes.nav.main'))
+                @include('pulsar::includes.nav.main')
+            @endif
+        @else
+            @if(session('userAcl')->allows($package->folder_012, 'access') && View::exists($package->folder_012 . '::includes.nav.main'))
+                @include($package->folder_012 . '::includes.nav.main')
+            @endif
         @endif
     @endforeach
 </ul>
