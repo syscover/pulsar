903ddc096451bac3942a91bea1a2cfc811aef31a
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-03-21 18:53:49 +0100

    minor fix

--- src/database/migrations/updates/2002_01_01_000008_pulsar_updateV8.php
+++ src/database/migrations/updates/2002_01_01_000008_pulsar_updateV8.php
@@ -3,17 +3,18 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Syscover\Pulsar\Models\Resource;
+use \Syscover\Pulsar\Models\Package;
 
 class PulsarUpdateV8 extends Migration
 {
 	/**
- * Run the migrations.
- *
- * @return void
- */
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
 	public function up()
 	{
-		if(Resource::where('id_007', 'admin-attachment-mime')->count() == 0)
+		if(Resource::where('id_007', 'admin-attachment-mime')->count() == 0 && Package::where('id_012', 2)->count() == 1)
 		{
 			Resource::create([
 				'id_007' 		=> 'admin-attachment-mime',
