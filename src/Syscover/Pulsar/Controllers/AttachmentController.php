e270f49760390635e606e51d8922311e8e026588
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-03-24 12:10:23 +0100

    delete docs

--- src/Syscover/Pulsar/Controllers/AttachmentController.php
+++ src/Syscover/Pulsar/Controllers/AttachmentController.php
@@ -145,7 +145,7 @@ class AttachmentController extends Controller {
     {
         $parameters = $request->route()->parameters();
 
-        $attachment = Attachment::getTranslationRecord(['id' => $parameters['id'], 'lang' => $parameters['lang']]);
+        $attachment = Attachment::getTranslationRecord['id' => $parameters['id'], 'lang' => $parameters['lang']]);
 
         if($attachment->file_name_016 != null && $attachment->file_name_016 != "")
         {
