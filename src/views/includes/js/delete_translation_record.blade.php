c8a2802b64c74c7360a8f87680490dec6b810894
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-03-28 16:58:08 +0200

    fix delete translation js

--- src/views/includes/js/delete_translation_record.blade.php
+++ src/views/includes/js/delete_translation_record.blade.php
@@ -1,26 +1,28 @@
-<!-- pulsar::includes.js.delete_translation_record -->
-<script>
-    $(document).ready(function() {
-        $('.delete-lang-record').on('click', function() {
+@if(isset($id))
+    <!-- pulsar::includes.js.delete_translation_record -->
+    <script>
+        $(document).ready(function() {
+            $('.delete-lang-record').on('click', function() {
 
-            var that = this
+                var that = this
 
-            $.msgbox('{!! trans('pulsar::pulsar.message_delete_translation_record', ['id' => $id]) !!}',
-                {
-                    type:'confirm',
-                    buttons: [
-                        {type: 'submit', value: '{{ trans('pulsar::pulsar.accept') }}'},
-                        {type: 'cancel', value: '{{ trans('pulsar::pulsar.cancel') }}'}
-                    ]
-                },
-                function(buttonPressed) {
-                    if(buttonPressed=='{{ trans('pulsar::pulsar.accept') }}')
+                $.msgbox('{!! trans('pulsar::pulsar.message_delete_translation_record', ['id' => $id]) !!}',
                     {
-                        $(location).attr('href', $(that).data('delete-url'));
+                        type:'confirm',
+                        buttons: [
+                            {type: 'submit', value: '{{ trans('pulsar::pulsar.accept') }}'},
+                            {type: 'cancel', value: '{{ trans('pulsar::pulsar.cancel') }}'}
+                        ]
+                    },
+                    function(buttonPressed) {
+                        if(buttonPressed=='{{ trans('pulsar::pulsar.accept') }}')
+                        {
+                            $(location).attr('href', $(that).data('delete-url'));
+                        }
                     }
-                }
-            )
+                )
+            })
         })
-    })
-</script>
-<!-- ./pulsar::includes.js.delete_translation_record -->
+    </script>
+    <!-- ./pulsar::includes.js.delete_translation_record -->
+@endif
