047e7ed41dc833c0d50a0e8249b0b5fe932c8575
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-03-30 16:41:40 +0200

    update form file group

--- src/views/includes/html/form_file_group.blade.php
+++ src/views/includes/html/form_file_group.blade.php
@@ -1,7 +1,7 @@
 <!-- pulsar::includes.html.form_file_group -->
 <div class="form-group">
-    <label class="col-md-2 control-label">{{ $label }} @if(isset($required)) @include('pulsar::includes.html.required') @endif</label>
-    <div class="col-md-10">
+    <label class="col-md-{{ $labelSize or 2 }} control-label">{{ $label }} @if(isset($required)) @include('pulsar::includes.html.required') @endif</label>
+    <div class="col-md-{{ $fieldSize or 10 }}">
         <div class="fileinput fileinput-new input-group" data-provides="fileinput">
             <div class="form-control" data-trigger="fileinput"><i class="glyphicon glyphicon-file fileinput-exists"></i> <span class="fileinput-filename"></span></div>
             <span class="input-group-addon btn btn-default btn-file"><span class="fileinput-new">Select file</span><span class="fileinput-exists">Change</span><input type="file" name="{{ $name }}"{{ isset($required)? ' required' : null }}></span>
