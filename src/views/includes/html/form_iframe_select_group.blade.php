4bd82eabfb292a709d3a09d776bd0561733ce076
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-03-30 18:00:41 +0200

    minor fix

--- src/views/includes/html/form_iframe_select_group.blade.php
+++ src/views/includes/html/form_iframe_select_group.blade.php
@@ -2,10 +2,10 @@
     <label class="col-md-{{ $labelSize or 2 }} control-label">{{ $label }} @if(isset($required) && $required) @include('pulsar::includes.html.required') @endif</label>
     <div class="col-md-{{ $fieldSize or 8 }}">
         <input class="form-control" type="{{ isset($type)? $type : 'text' }}" name="{{ $name }}" value="{{ isset($value)? $value : null }}"{!! isset($data)? Miscellaneous::setDataAttributes($data) : null !!} @if(isset($placeholder)) placeholder="{{ $placeholder }}"@endif @if(isset($maxLength)) maxlength="{{ $maxLength }}"@endif @if(isset($rangeLength))rangelength="{{ $rangeLength }}"@endif @if(isset($min)) min="{{ $min }}"@endif{{ isset($readOnly) && $readOnly? ' readonly' : null }}{{ isset($required) && $required? ' required' : null }}>
-        {!! $errors->first($name.'id', config('pulsar.errorDelimiters')) !!}
+        {!! $errors->first($name . 'Id', config('pulsar.errorDelimiters')) !!}
     </div>
     <div class="col-md-{{ $iconSize or 2 }}">
         <a href="{{ $modalUrl }}" class="magnific-popup"><span class="fa fa-plus"></span></a>
-        <input type="hidden" name="{{ $name.'id' }}" value="{{ isset($valueId)? $valueId : null }}"{{ isset($required) && $required? ' required' : null }}>
+        <input type="hidden" name="{{ $name . 'Id' }}" value="{{ isset($valueId)? $valueId : null }}"{{ isset($required) && $required? ' required' : null }}>
     </div>
 </div>
