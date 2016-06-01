f97795c66c064fc40da2bcba1bc6fed701ea474e
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-06-01 10:33:55 +0200

    set textarea-container

--- src/views/includes/html/form_textarea_group.blade.php
+++ src/views/includes/html/form_textarea_group.blade.php
@@ -1,4 +1,4 @@
-<div class="form-group">
+<div class="form-group textarea-container">
     <label class="col-md-{{ $labelSize or 2 }} control-label">{{ $label }} @if(isset($required) && $required) @include('pulsar::includes.html.required') @endif</label>
     <div class="col-md-{{ $fieldSize or 10 }}">
         <textarea class="form-control" name="{{ $name }}" @if(isset($rows))rows="{{ $rows }}"@endif @if(isset($cols)) cols="{{ $cols }}"@endif{{ isset($readOnly) && $readOnly? ' readonly' : null }}{{ isset($required) && $required? ' required' : null }}>{{ isset($value)? $value : null }}</textarea>
