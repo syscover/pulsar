bb856a90570d45e175c22a5037944bceb44aa6bf
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-09-03 13:03:26 +0200

    minor fix

--- src/Syscover/Pulsar/Controllers/Auth/PasswordController.php
+++ src/Syscover/Pulsar/Controllers/Auth/PasswordController.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 use Illuminate\Mail\Message;
