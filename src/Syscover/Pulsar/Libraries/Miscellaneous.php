060aefce15bd963b2706dadeceaad02ea92a5f9b
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-09-23 10:39:58 +0200

    add function to convert string to float

--- src/Syscover/Pulsar/Libraries/Miscellaneous.php
+++ src/Syscover/Pulsar/Libraries/Miscellaneous.php
@@ -795,4 +795,28 @@ class Miscellaneous
 
         return $response;
     }
+
+    /**
+     * Function to convert string number with dot and comma, in float
+     *
+     * @param   $num
+     * @return  float
+     */
+    public static function tofloat($num)
+    {
+        $dotPos     = strrpos($num, '.');
+        $commaPos   = strrpos($num, ',');
+        $sep = (($dotPos > $commaPos) && $dotPos) ? $dotPos :
+            ((($commaPos > $dotPos) && $commaPos) ? $commaPos : false);
+
+        if (!$sep)
+        {
+            return floatval(preg_replace("/[^0-9]/", "", $num));
+        }
+
+        return floatval(
+            preg_replace("/[^0-9]/", "", substr($num, 0, $sep)) . '.' .
+            preg_replace("/[^0-9]/", "", substr($num, $sep+1, strlen($num)))
+        );
+    }
 }
