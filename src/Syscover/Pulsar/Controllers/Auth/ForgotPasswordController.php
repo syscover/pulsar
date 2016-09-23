ef45a40496d802802e268171e6c7a5a50610e0d3
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-09-23 14:05:12 +0200

    remove resetNOtifier method

--- src/Syscover/Pulsar/Controllers/Auth/ForgotPasswordController.php
+++ src/Syscover/Pulsar/Controllers/Auth/ForgotPasswordController.php
@@ -34,7 +34,7 @@ class ForgotPasswordController extends Controller
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
         $response = $this->broker()->sendResetLink(
-            $request->only('email'), $this->resetNotifier()
+            $request->only('email')
         );
 
         switch ($response) {
