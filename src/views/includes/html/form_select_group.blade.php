f161298837c9b12e1e0b076a24983da0092e5fd3
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-03-31 20:54:38 +0200

    minor fix

--- src/views/includes/html/form_select_group.blade.php
+++ src/views/includes/html/form_select_group.blade.php
@@ -2,7 +2,9 @@
     <label{!! isset($labelId)? ' id="' . $labelId . '"' : null !!} class="col-md-{{ $labelSize or 2 }} control-label">{{ isset($label)? $label : null }} @if(isset($required) && $required) @include('pulsar::elements.required') @endif</label>
     <div class="col-md-{{ $fieldSize or 10 }}">
         <select{!! isset($id)? ' id="' . $id . '" ' : null !!} class="form-control {{ $class or null }}" {!! isset($style)? ' style="' . $style . '" ' : null !!}name="{{ $name }}"{{ isset($multiple)? ' multiple' : null }}{{ isset($required) && $required? ' required' : null }}{{ isset($disabled) && $disabled? ' disabled' : null }}{!! isset($data)? Miscellaneous::setDataAttributes($data) : null !!}>
-            @if(!isset($multiple))<option value="">{{ trans('pulsar::pulsar.select_a') }} {{ isset($label)? $label : null }}</option>@endif
+            @if(! isset($multiple))
+                <option value="">{{ trans('pulsar::pulsar.select_a') }} {{ isset($label)? $label : null }}</option>
+            @endif
             @if(isset($objects))
                 @foreach ($objects as $object)
                     @if(is_array($object))
