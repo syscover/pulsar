1e80e4aaf3d128f9e6a02e68136730c1c0f30268
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-11-27 22:36:47 +0100

    minor fix

--- src/views/includes/html/form_text_group.blade.php
+++ src/views/includes/html/form_text_group.blade.php
@@ -7,9 +7,9 @@
     @if(isset($inputs) && is_array($inputs))
         @foreach($inputs as $input)
             @if(isset($input['label']))
-                <label class="col-md-{{ $labelSize or 2 }} control-label">{{ $input['label'] }} @if(isset($input['required']) && $input['required']) @include('pulsar::elements.required') @endif</label>
+                <label class="col-md-{{ $labelSize ?? 2 }} control-label">{{ $input['label'] }} @if(isset($input['required']) && $input['required']) @include('pulsar::elements.required') @endif</label>
             @endif
-            <div class="col-md-{{ $input['fieldSize'] or 10 }}">
+            <div class="col-md-{{ $input['fieldSize'] ?? 10 }}">
                 <input class="{{ isset($input['class'])? ' ' . $input['class'] : 'form-control' }}" type="{{ $input['type'] or 'text' }}" name="{{ $input['name'] }}" value="{{ $input['value'] or null }}"{!! isset($input['data'])? Miscellaneous::setDataAttributes($input['data']) : null !!} @if(isset($input['placeholder'])) placeholder="{{ $input['placeholder'] }}"@endif @if(isset($input['maxLength'])) maxlength="{{ $input['maxLength'] }}"@endif @if(isset($input['rangeLength']))rangelength="{{ $input['rangeLength'] }}"@endif @if(isset($input['min'])) min="{{ $input['min'] }}"@endif @if(isset($max)) max="{{ $max }}"@endif{{ isset($input['readOnly']) && $input['readOnly']? ' readonly' : null }}{{ isset($input['required']) && $input['required']? ' required' : null }}{{ isset($input['checked']) && $input['checked']? ' checked' : null }}>
                 {!! $errors->first($input['name'], config('pulsar.errorDelimiters')) !!}
             </div>
